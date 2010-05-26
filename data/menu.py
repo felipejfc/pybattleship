2bde93f5c5c5104dcf7d2e67b8dc0261fb011d2f
filipealencar <filipe.69@gmail.com>
2010-05-26 19:07:30 -0300

    atualizacao

--- data/menu.py
+++ data/menu.py
@@ -4,24 +4,38 @@ from pygame.locals import *
 from functions import *  #importa as funcoes do functions.py
 
 class Menu(object):
-	def __init__(self, screen):
-		clickable = {} #cria um dicionario de elementos clicaveis na tela.
-		self.screen = screen  #recebe screen do main.py
-		background = pygame.image.load(filepath("menu.jpg")) #Carrega BG
-		ng_button = pygame.image.load(filepath("newgamebutton.png")) #Carrega NB
-		clickable[ng_button] = (21,420) #poe a posicao do elemento no dict
-		print clickable.values()
-		
-		while True:             #Loop do menu, que pega os eventos.
-			for evento in pygame.event.get():
-				if evento.type == pygame.QUIT:
-					exit()
-				if evento.type == MOUSEBUTTONDOWN: #Verifica onde o usurio clicou.
-					if evento.button == 1:
-						for key,value in clickable.items():
-							if checkclick(value,key.get_size(),evento.pos) and key==ng_button:
-								print "Novo Jogo!"								
-			
-			self.screen.blit(background, (0, 0)) #Desenha o botao newgame na tela
-			self.screen.blit(ng_button, (21, 420)) #Desenha o BG
-			pygame.display.flip() #Atualiza a Tela
+    def __init__(self, screen):
+        clickable = {} #cria um dicionario de elementos clicaveis na tela.
+        self.screen = screen  #recebe screen do main.py
+        background = pygame.image.load(filepath("menu.jpg")) #Carrega BG
+        ng_button = pygame.image.load(filepath("newgamebutton.png")) #Carrega NB
+        ng_button_vermelho = pygame.image.load(filepath("newgamebutton_s.png")) #Carrega NB_VERMELHO
+        clickable[ng_button] = (21,420) #poe a posicao do elemento no dict
+        print clickable.values()
+        
+        while True:              #Loop do menu, que pega os eventos.
+            no_botao = False
+            for evento in pygame.event.get():
+                print evento
+                if evento.type == pygame.QUIT:
+                    exit()
+                if evento.type == MOUSEMOTION:
+                    for key,value in clickable.items():
+                        if checkclick(value,key.get_size(),evento.pos):
+                            no_botao = True       
+                if evento.type == MOUSEBUTTONDOWN: #Verifica onde o usurio clicou.
+                    if evento.button == 1:
+                        for key,value in clickable.items():
+                            if checkclick(value,key.get_size(),evento.pos) and key==ng_button:
+                                print "Novo jogo"
+                                
+            if no_botao == True:
+                 self.screen.blit(ng_button_vermelho, (21, 420))
+                 pygame.display.flip()
+            else:
+                 self.screen.blit(background, (0, 0)) #Desenha o BG
+                 self.screen.blit(ng_button, (21, 420)) #Desenha o botao newgame na tela
+            pygame.display.flip() #Atualiza a Tela
+                                
+                                                                
+
